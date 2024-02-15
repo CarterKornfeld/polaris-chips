917d0e733f989450fd507c1092778d48a1f02962
CarterKornfeld <148253614+CarterKornfeld@users.noreply.github.com>
2024-02-15 14:27:59 -0500

    I did activity 6

--- src/counter-app.js
+++ src/counter-app.js
@@ -9,45 +9,174 @@ export class CounterApp extends LitElement {
  
   constructor() {
     super();
-
+    this.nums = 1;
+    this.min = 0;
+    this.max = 10;
   }
 
+   
   
+
+
   static get styles() {
     
     return css`
-   
-      :host {
-        /* Always make sure that your element has a default way of being displayed */
-        display: inline-flex;
+   #cardlist
+{
+  display:inline-flex;
+}
 
-      }
+#confetti
+{
+  height: 100%;
+  width: 100%;
+}
 
-      span{
-        background-color: blue;
-        color: black;
-        font-size: 24px;
-        padding: 16px;
-        margin: 8px;
-      }
+.card {
+  padding: 16px;
+  margin: 16px;  
+  background-color: orange;
+  
+}
 
-      span:hover{
-        background-color: grey;
-        border: 1px solid black;
-      }
+
+.heading{
+  font-size: 40px;
+  color: black;
+  text-align: center;
+}
+
+
+.minus{
+  background-color: blue;
+  color : white;
+  font-size: 28px;
+  border-radius:10%;
+  margin: 8px 16px
+  
+  
+}
+
+.plus{
+  background-color: blue;
+  color : white;
+  font-size: 28px;
+  border-radius:10%;
+  margin: 8px 16px
+  
+  
+}
+.plus:focus,  
+.plus:hover{
+  background-color: lightgreen;
+}
+
+.minus:focus,  
+.minus:hover{
+  background-color: lightgreen;
+}
+
+
+
 
     `;
   }
+
+
+  plusClick() 
+  {
+    this.nums++;
+
+  }
+
+  minusClick()
+  {
+    this.nums--;
+  }
+
+ 
+updated(changedProperties) {
+  if (changedProperties.has('nums') && this.nums === 21) {
+    this.makeItRain();
+  }
+}
+
+makeItRain() {
+  // this is called a dynamic import. It means it won't import the code for confetti until this method is called
+  // the .then() syntax after is because dynamic imports return a Promise object. Meaning the then() code
+  // will only run AFTER the code is imported and available to us
+  import("@lrnwebcomponents/multiple-choice/lib/confetti-container.js").then(
+    (module) => {
+      // This is a minor timing 'hack'. We know the code library above will import prior to this running
+      // The "set timeout 0" means "wait 1 microtask and run it on the next cycle.
+      // this "hack" ensures the element has had time to process in the DOM so that when we set popped
+      // it's listening for changes so it can react
+      setTimeout(() => {
+        // forcibly set the poppped attribute on something with id confetti
+        // while I've said in general NOT to do this, the confetti container element will reset this
+        // after the animation runs so it's a simple way to generate the effect over and over again
+        this.shadowRoot.querySelector("#confetti").setAttribute("popped", "");
+      }, 0);
+    }
+  );
+}
+  
   render() {
+
+
+    var container = "";
+    if(this.nums === 18)
+    {
+      container = 'red';
+    }
+    else if(this.nums === 21)
+    {
+      container = 'blue';
+    }
+    else if(this.nums == this.max)
+    {
+      container = "green";
+    }
+    else if(this.nums == this.min)
+    {
+      container = "white";
+    }
+    else
+    {
+      container = 'Purple';
+    }
+
+
     
-    return html`<a href="${this.link}"><span>${this.title}</span></a>`;
+    
+    
+    return html`
+    
+  
+  <div id="cardlist">
+  <confetti-container id="confetti">
+
+  <div class="card">
+    <div class="heading" style='color:${container}'>${this.nums}</div>
+    <button class="plus" @click = ${this.plusClick} ?disabled="${this.max === this.nums}">+</button>
+    <button class="minus"  @click="${this.minusClick}" ?disabled="${this.min === this.nums}">-</button>
+  </div>
+
+  </confetti-container>
+  </div>
+
+
+
+  </div>`;
   }
 
 
-  static get properties() {
+  static get properties() 
+  {
     return {
-      
-      title: { type: String },
+      nums: { type: Number, Reflect: true },
+      min: { type: Number},
+      max: {type: Number},
     };
   }
 }
