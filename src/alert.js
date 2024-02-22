bfc51b5749d6174ca818700fbd5fd3c50c508016
CarterKornfeld <148253614+CarterKornfeld@users.noreply.github.com>
2024-02-22 13:30:18 -0500

    h

--- src/alert.js
+++ src/alert.js
@@ -1,1 +1,47 @@
 
+import { LitElement, html, css } from 'lit';
+
+export class Alert extends LitElement {
+
+  static get tag() {
+    return 'alert-system';
+  }
+ 
+  constructor() {
+    super();
+    
+  }
+
+  static get styles() {
+    
+    return css`
+    .container
+    {
+    width: 100%;
+    height: 16px;
+    background-color: blue;
+    }
+
+    `;
+  }
+
+  render() {
+    return html`
+    
+    <div class="container"></div>
+
+
+  </div>`;
+  }
+
+
+  static get properties() 
+  {
+    return {
+      
+    };
+  }
+}
+
+globalThis.customElements.define(Alert.tag, Alert);
+
