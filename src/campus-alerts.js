438f49b48c85f11fb351c012b0f66836404a2f74
CarterKornfeld <148253614+CarterKornfeld@users.noreply.github.com>
2024-02-29 16:58:29 -0500

    h

--- src/campus-alerts.js
+++ src/campus-alerts.js
@@ -10,18 +10,27 @@ export class CampusAlerts extends LitElement {
  
   constructor() {
     super();
+    this.sticky;
+    this.message = "";
+    this.issueLevel="welcome"
     this.date = "Febuary 27th 2024";
-    this.alertMessage = "This is an alert";
     this.open =false;
   }
 
-   
   
 
 
+  
   static get styles() {
     
     return css`
+
+    .sticky{
+        position: sticky;
+        top: 0;
+        z-index: 100;
+        opacity: 1.0;
+      }
 .opencontainer {
             display: flex;
             background-color:orange;
@@ -66,38 +75,39 @@ export class CampusAlerts extends LitElement {
           align-items: center;
           justify-content: center;
           padding:10px;
-           background-color:orange;
+          
         }
         .open-toggle-button
         {
           display:flex;
           align-items: center;
           justify-content: center;
-           background-color:orange;
+          
         }
-   
-    `;
-  }
+
+        
+    
+     `;}
 
 toggleAlert()
 {
 this.open = !this.open;
 }
 
- openView()
+ openView(colorP, colorB)
  {
   return html`
 
-<div class="opencontainer">
-        <div class="box1"> ${this.date}</div>
-        <div class="box2 ">
-          <div class = "alertText">
-            Hello
+<div class="opencontainer ${(this.sticky) ? "sticky" : ""}" style = 'background-color:${colorP} '  >
+        <div class="box1" style = 'background-color:${colorP} '> ${this.date}</div>
+        <div class="box2 " style= 'background-color:${colorB} '>
+          <div  class = "alertText">
+            ${this.message}
           </div>
         </div>
-        <div class="box3">
-        <div class="open-toggle-button" @click="${this.toggleAlert}">
-          ${this.open ? 'Close' : 'Open'} Alert
+        <div class="box3" style= 'background-color:${colorP}'>
+        <div tabindex="0"  class="open-toggle-button" @click="${this.toggleAlert}"  style= 'background-color:${colorP}'>
+          ${this.open ? 'Close' : 'Open'} 
           <svg xmlns="http://www.w3.org/2000/svg" style=" height:35px; width:35px;" viewBox="0 0 24 24"><title>chevron-down</title><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
 
         </div>
@@ -107,14 +117,15 @@ this.open = !this.open;
  
   </div>`;
  }
- closeView()
+
+ closeView(colorP)
  {
   return html`
 
-<div class="closedContainer">
-        <div class="close-toggle-button" @click="${this.toggleAlert}">
+<div class="closedContainer ${(this.sticky) ? "sticky" : ""}" style= 'background-color:${colorP} '  >
+        <div tabindex="0" class="close-toggle-button" @click="${this.toggleAlert}" >
         <svg xmlns="http://www.w3.org/2000/svg" style=" height:50px; width:50px;" viewBox="0 0 24 24"><title>alert-circle-outline</title><path d="M11,15H13V17H11V15M11,7H13V13H11V7M12,2C6.47,2 2,6.5 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4A8,8 0 0,1 20,12A8,8 0 0,1 12,20Z" /></svg>
-           Alert
+           ${this.issueLevel}
            <svg xmlns="http://www.w3.org/2000/svg" style=" height:35px; width:35px;" viewBox="0 0 24 24"><title>chevron-down</title><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
         </div>
 
@@ -124,13 +135,29 @@ this.open = !this.open;
  }
   
   render() {    
-    return (this.open) ? this.openView() : this.closeView();
+      let colorP = "white";
+      let colorB= "black"
+      if(this.issueLevel === "notice"){colorP = "yellow", colorB = "orange"}
+      if(this.issueLevel === "warning"){colorP = "red", colorB = "green"}
+      if(this.issueLevel === "alert"){colorP = "purple", colorB = "pink"}
+      if(this.issueLevel === "welcome"){colorP = "#cfeceb", colorB = "#fff"}
+
+      if(this.issueLevel === "welcome") (this.message) = "Hello Happy PSU!";
+        if(this.issueLevel === "notice") (this.message) = "Watch out possible oopsie";
+        if(this.issueLevel === "warning") (this.message) = "WARNING WARNING WARNING"
+        if(this.issueLevel === "alert") (this.message) = "GTFO DANGER IMMENENT WE ARE ALL GOING TO DIE";
+
+
+    return (this.open) ? this.openView(colorP,colorB) : this.closeView(colorP);
   }
 
 
   static get properties() 
   {
     return {
+      issueLevel:{type: String, reflect: true},
+      message:{type: String},
+      sticky:{type: Boolean},
       open: { type: Boolean, reflect: true },
       date: { type: String },
     };
